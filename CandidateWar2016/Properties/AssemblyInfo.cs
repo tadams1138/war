c9ce6d1d38e626f6ec94fa2d35fa083e3ff2cc77
Tom Adams <tadams1138@hotmail.com>
2015-12-25 00:53:37 -0500

    adding in angular site for candidate war2016

--- CandidateWar2016/Properties/AssemblyInfo.cs
+++ CandidateWar2016/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bce13572-d770-44f2-8fa9-ae7327744f01")]
+[assembly: Guid("184d9e03-2552-4f77-b0d6-c09a1a105edc")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
