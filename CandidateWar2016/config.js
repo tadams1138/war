b2090057323a3af7cf57980b006f0c7ead4f3669
Tom Adams <tadams1138@hotmail.com>
2015-12-28 01:33:09 -0500

    fix spelling, wording, formatting

--- CandidateWar2016/config.js
+++ CandidateWar2016/config.js
@@ -1,6 +1,6 @@
 ﻿(function () {
-    //angular.module('app').value('serviceRoot' , 'http://tandisoftwarapi.azurewebsites.net/api/War/2/');
-    angular.module('app').value('serviceRoot', 'http://tandisoftwarapitest.azurewebsites.net/api/War/2/');
+    angular.module('app').value('serviceRoot' , 'https://tandisoftwarapi.azurewebsites.net/api/War/2/');
+    //angular.module('app').value('serviceRoot', 'https://tandisoftwarapitest.azurewebsites.net/api/War/2/');
     //angular.module('app').constant('serviceRoot', 'http://localhost:6903/api/War/2/')
 })()
 
