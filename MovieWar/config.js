31006c7185514a58af53c05e020d89f2a62bf820
Tom Adams <tadams1138@hotmail.com>
2016-01-30 18:01:00 -0500

    initial add of movie war

---
+++ MovieWar/config.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    //var domain = 'https://tandisoftwarapi.azurewebsites.net/';
+    var domain = 'https://tandisoftwarapitest.azurewebsites.net/';
+    //var domain = 'http://localhost:6903/';
+
+    var serviceRoot = domain + 'api/War/1/';
+
+    angular.module('app')
+        .config(configure)
+        .value('serviceRoot', serviceRoot)
+        .value('domain', domain)
+
+    configure.$inject = ['$httpProvider'];
+    function configure($httpProvider) {
+        $httpProvider.defaults.withCredentials = true;
+    }
+})()
+
